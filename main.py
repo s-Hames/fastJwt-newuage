c470443d769cd0574e4a9f2af53dd80f3f4b58f8
Hames-1616 <bilquis748@gmail.com>
2024-08-18 22:02:14 +0530

    📦 NEW: main files changes

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from fastapi import FastAPI
 import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app")
-    # uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("app.api:app")
+#     # uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
