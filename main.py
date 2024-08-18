ef1905d1099f60715eb682504fb303b1d770098d
Hames-1616 <bilquis748@gmail.com>
2024-08-18 22:08:00 +0530

    📦 NEW: more main changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 
 if __name__ == "__main__":
     # uvicorn.run("app.api:app")
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8081)
+    uvicorn.run("app.api:app")
 
 # app = FastAPI()
 
